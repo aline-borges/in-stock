df920f340c7359b05cab06269db7446f62a7977c
aline-borges <ap.borges@live.com>
2021-11-16 01:47:09 -0300

    feat(header): create a header

---
+++ src/components/organisms/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+import Logo from '../../atoms/logo/index'
+
+const Header = () => {
+  return(
+    <header className="main-header">
+      <Logo />
+    </header>
+  )
+}
+
+export default Header 
